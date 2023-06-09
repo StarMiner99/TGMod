0e8c9346aabfa559f908dc46dd765b570d9fd66e
thijs <thijs@licht51.de>
2023-06-09 14:08:44 +0200

    added mik design files added pathfinding mik and changed attributes

--- src/main/java/com/github/tgmuk/tgitmod/entity/MikEntity.java
+++ src/main/java/com/github/tgmuk/tgitmod/entity/MikEntity.java
@@ -8,7 +8,11 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -20,20 +24,25 @@ public class MikEntity extends Animal {
         super(entityType, level);
     }
 
-    @SubscribeEvent
-    public void ooohITouchedIt(PlayerInteractEvent.EntityInteract event) {
-        this.convertTo(EntityType.CAMEL, true);
-    }
-
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob mob) {
         return EntityTypeRegister.MIK_ENTITY.get().create(level);
     }
 
-
     public static AttributeSupplier.Builder getMikEntityAttributes() {
-        return Mob.createMobAttributes().add(Attributes.JUMP_STRENGTH, 2f).add(Attributes.MAX_HEALTH, 15);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 15D).add(Attributes.MOVEMENT_SPEED, 0.25D);
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
+        this.goalSelector.addGoal(3, new BreedGoal(this, 1.0D));
+        this.goalSelector.addGoal(5, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
     }
 
 
